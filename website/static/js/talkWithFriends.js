1421633d19834ae62208c2dc77619fda4cf49d52
Lucas <lucas.m.loberto@gmail.com>
2025-01-16 16:28:57 +0000

    agora sim

--- website/static/js/talkWithFriends.js
+++ website/static/js/talkWithFriends.js
@@ -3,10 +3,10 @@ function scrollToBottom() {
     chatBox.scrollTop = chatBox.scrollHeight;
 }
 
-// Garante que o scroll vai para o fim ao carregar a página
+
 window.onload = scrollToBottom;
 
-// Opcional: Garante que o scroll vai para o fim ao adicionar novas mensagens
+
 const observer = new MutationObserver(scrollToBottom);
 const chatBox = document.querySelector(".chat-box");
 observer.observe(chatBox, { childList: true });
